4554be68127c8bcc3b9baad4dc8e9d04290fdb8f
Luan Carminatti <luancarminatt@gmail.com>
2025-06-20 17:39:47 -0300

    ajuste nos nomes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const postRoutes = require('./routes/post.routes');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello my friend! API Rodando...')
 });
+
+app.use('/post', postRoutes);
+
 
 app.listen(PORT, () => {
    console.log(`Servidor rodando local em: http://localhost:${PORT}`); 
