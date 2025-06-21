43dbbb488d76452c23eb49e2ec3ddf7919a431a1
Luan Carminatti <luancarminatt@gmail.com>
2025-06-21 15:35:46 -0300

    alterando a rota do método http get para post

---
+++ controllers/post.js
@@ -0,0 +1,24 @@
+const { criarPost } = require('../models/post.js');
+
+// CRUD
+
+// Create
+const criarNovoPost = (req, res) => {
+    const { titulo, conteudo, autor } = req.body;
+
+    if (!titulo || !conteudo || !autor) {
+        return res.status(400).json({ error: 'Preencha todos os campos obrigatórios.' });
+    }
+
+    try {
+        const id = criarNovoPost(titulo, conteudo, autor);
+        res.status(201).json({ message: 'Post criado com sucesso!', id });
+    } catch (error) {
+        console.error('Erro ao criar post:', error.message);
+        res.status(500).json({ error: 'Erro interno ao criar post.' });
+    }
+}
+
+module.exports = {
+  criarNovoPost
+};
